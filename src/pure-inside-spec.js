4af269d67ae090ecc5bafec21934ae318ec609a5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-08 08:34:53 -0400

    chore(test): simple unit test

--- src/pure-inside-spec.js
+++ src/pure-inside-spec.js
@@ -1,7 +1,16 @@
 'use strict'
 
 /* global describe, it */
-describe('', () => {
-  it('', () => {
+describe('add', () => {
+  const add = require('../examples/add')
+  it('adds numbers', () => {
+    console.assert(add(2, 3) === 5)
+  })
+})
+
+describe('sub', () => {
+  const sub = require('../examples/sub')
+  it('subtracts numbers', () => {
+    console.assert(sub(2, 3) === -1)
   })
 })
